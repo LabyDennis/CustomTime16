1c113f6c725c6e5cd4206f5951a8fe05752d8c17
Dennis <dennisspeznas@gmail.com>
2022-03-11 17:00:14 +0100

    Initial commit

--- src/main/java/de/speznas/customtime/mixin/CustomTimeMixin.java
+++ src/main/java/de/speznas/customtime/mixin/CustomTimeMixin.java
@@ -2,7 +2,6 @@ package de.speznas.customtime.mixin;
 
 import de.speznas.customtime.timings.TimeController;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,8 +14,6 @@ public abstract class CustomTimeMixin {
 
   @Shadow @Final private ClientWorld.ClientWorldInfo field_239130_d_;
 
-  @Shadow public abstract ClientWorld.ClientWorldInfo getWorldInfo();
-
   @Inject(method = "setDayTime", at = @At("HEAD"), cancellable = true)
   private void injectSetDayTime(CallbackInfo ci) {
     if(TimeController.getInstance().isEnabled()){
@@ -27,7 +24,6 @@ public abstract class CustomTimeMixin {
   @Inject(method = "tick", at = @At("TAIL"))
   private void injectTick(CallbackInfo ci){
     if(TimeController.getInstance().isEnabled()){
-      getWorldInfo().getGameRulesInstance().get(GameRules.DO_DAYLIGHT_CYCLE).set(true, null);
 
       TimeController.getInstance().callUpdate();
       field_239130_d_.setDayTime(TimeController.getInstance().getCustomTime());
